0dbf760d7bc112f26882745d857c39711ed0a4e9
lin5188 <33708771+lin5188@users.noreply.github.com>
2019-05-15 22:12:57 +0800

    Create DayDayUp.py

---
+++ DOC/Python/Python_Note/实例/DayDayUp.py
@@ -0,0 +1,15 @@
+'''
+#DayDayUp.py
+def dayup(df):
+    dayup = 1
+    for i in range(365):
+        if i % 7 in [6,0]:
+            dayup = dayup * (1 - 0.01)
+        else:
+            dayup = dayup * (1 + df)
+    return dayup
+dayf = 0.001
+while dayup(dayf) < 37.87:
+    dayf += 0.001
+print("工作日努力的参数{:.3f}".format(dayf))
+'''
