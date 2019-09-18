780a42e362ed9c6980172d7e73f70b9953ab09b4
lin5188 <33708771+lin5188@users.noreply.github.com>
2019-09-18 18:50:42 +0800

    del idea

--- DOC/Python/代码/login1_url.py
+++ DOC/Python/代码/login1_url.py
@@ -7,5 +7,5 @@ headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36
 
 response = requests.get(url,headers=headers)
 
-with open("renren1.html","w",encoding="utf-8") as f:
+with open("renren3.html","w",encoding="utf-8") as f:
     f.write(response.content.decode())
